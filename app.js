3e084989d17bd9116c223d4ff47f3dee91f17fc8
nkhanhng97@gmail.com <khanhmatden123@gmail.com>
2018-06-07 14:45:09 +0700

    add round

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+var gameID;
+
 app.post('/api/game',(req,res)=>{
     console.log(req.body)
     const newGame = {
@@ -30,7 +32,10 @@ app.post('/api/game',(req,res)=>{
     console.log(newGame);
     Game.create(newGame,(err, gameCreated)=>{
         if (err) res.status(500).send({ success: 0, err })
-        else res.send({ success: 1, gameId: gameCreated._id })
+        else{
+            gameID = gameCreated._id;
+            res.send({ success: 1, gameId: gameCreated._id })
+        } 
     });
 })
 
@@ -49,7 +54,45 @@ app.get('/game/:id', (req, res) => {
     })
 })
 
+app.post('/api/round',(req,res)=>{
+    Game.findById(gameID,(err,game)=>{
+        if(err) throw err;
+        let player1 = game.player[0]
+        let player2 = game.player[1]
+        let player3 = game.player[2]
+        let player4 = game.player[3]
 
+        let score1 = parseInt(req.body.p1s) 
+        let score2 = parseInt(req.body.p2s)
+        let score3 = parseInt(req.body.p3s)
+        let score4 = parseInt(req.body.p4s)
+
+        console.log(score1)
+
+        game.round.push({
+            score: [{
+                player: player1,
+                pscore :score1
+            },
+            {
+                player: player2,
+                pscore :score2
+            },
+            {
+                player: player3,
+                pscore :score3
+            },
+            {
+                player: player4,
+                pscore :score4
+            }]
+        })
+
+        game.save((err)=>{
+            if(err) console.log("Error");
+        })
+    })
+})
 
 app.use(express.static('public'));
 
